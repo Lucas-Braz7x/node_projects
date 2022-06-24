2e3b3d9445f87219081a305de5016361a3d98819
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-06-23 21:29:26 -0300

    Create routes, middlewares and errors

--- API-Vendas/src/server.ts
+++ API-Vendas/src/server.ts
@@ -1,1 +1,16 @@
-console.log('Server is running...');
+import 'reflect-metadata';
+import express from 'express';
+//import axios from 'axios';
+import cors from 'cors';
+
+import routes from './shared/http/routes/index';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3080;
+
+app.listen(PORT, () => console.log(`Server is running in ${PORT}`));
