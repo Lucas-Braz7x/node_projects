20fa17bb9c1bfe8cadfce0bffb6c08f10d07b7f3
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-06-24 22:50:03 -0300

    Create validation with Celebrate

--- API-Vendas/src/server.ts
+++ API-Vendas/src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 //import axios from 'axios';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
+
 import '@shared/typeOrm';
 import routes from '@shared/http/routes/index';
 
@@ -11,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 const PORT = process.env.PORT || 3080;
 
--- API-Vendas/src/shared/http/routes/index.ts
+++ API-Vendas/src/shared/http/routes/index.ts
@@ -1,14 +1,55 @@
 import { ProductController } from '@modules/controller/ProductController';
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productController = new ProductController();
 
 const routes = Router();
 
 routes.get('/products', productController.index);
-routes.get('/products/:id', productController.show);
-routes.post('/products', productController.create);
-routes.put('/products/:id', productController.update);
-routes.delete('/products/:id', productController.delete);
+
+routes.get(
+  '/products/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+routes.post(
+  '/products',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+routes.put(
+  '/products/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string(),
+      price: Joi.number().precision(2),
+      quantity: Joi.number(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+routes.delete(
+  '/products/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default routes;
